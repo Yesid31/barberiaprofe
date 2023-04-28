e18a16f55dda6d02754208fcadfaa9e691411040
desarrollo <desarrollo@academicos.cesde.edu.co>
2023-04-28 10:07:33 -0500

    nuevo

--- src/Agenda/Agenda.jsx
+++ src/Agenda/Agenda.jsx
@@ -9,13 +9,17 @@ export function Agenda() {
                 <div className="row">
                     <div className="col-12 col-md-6">
                         <div className="input-group mb-3">
-                            <span className="input-group-text" id="basic-addon1">@</span>
+                            <span className="input-group-text" id="basic-addon1">
+                            <i className="bi bi-person-badge"></i>
+                            </span>
                             <input type="text" className="form-control" placeholder="Nombre Cliente" />
                         </div>
                     </div>
                     <div className="col-12 col-md-6">
                         <div className="input-group mb-3">
-                            <span className="input-group-text" id="basic-addon1">@</span>
+                            <span className="input-group-text" id="basic-addon1">
+                            <i className="bi bi-envelope-fill"></i>
+                            </span>
                             <input type="text" className="form-control" placeholder="Correo cliente" />
                         </div>
                     </div>
